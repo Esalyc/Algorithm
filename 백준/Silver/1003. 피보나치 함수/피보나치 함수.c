5b2eb21043e63225b4a4bde52b1530e09338f4e4
Esalyc <163393620+Esalyc@users.noreply.github.com>
2024-03-26 17:57:38 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int fbz(int n);
+int fbo(int n);
+int setz[41] = {0,};
+int seto[41] = {0,};
+
+int main(void) {
+  int N;
+  scanf("%d", &N);
+  for(int i = 0; i < N; i++) {
+    int n;
+    scanf("%d", &n);
+    printf("%d %d\n", fbz(n), fbo(n));
+  }
+
+  return 0;
+}
+
+int fbz(int n) {
+  switch(n) {
+    case(0): n = 1; break;
+    case(1): n = 0; break;
+    case(2): n = 1; break;
+    case(3): n = 1; break;
+    case(4): n = 2; break;
+    case(5): n = 3; break;
+    case(6): n = 5; break;
+    default: 
+      if(setz[n] != 0) n = setz[n];
+      else {
+        setz[n] = fbz(n - 1) + fbz(n - 2);
+        n = setz[n];
+      }
+  }
+  return n;
+}
+
+int fbo(int n) {
+  switch(n) {
+    case(0): n = 0; break;
+    case(1): n = 1; break;
+    case(2): n = 1; break;
+    case(3): n = 2; break;
+    case(4): n = 3; break;
+    case(5): n = 5; break;
+    case(6): n = 8; break;
+    default: 
+      if(seto[n] != 0) n = seto[n];
+      else {
+      seto[n] = fbo(n - 1) + fbo(n - 2);
+      n = seto[n];
+      }
+  }
+  return n;
+}
